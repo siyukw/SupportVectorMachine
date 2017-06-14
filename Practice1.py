a07042c977e93dffad394f577466966d1b9c692a
Siyu Kat Wang <siyukw@gmail.com>
2017-06-14 13:12:40 -0700

    changed file type

--- Practice1.py
+++ Practice1.py
@@ -1,6 +1,6 @@
-### import sys
-### from class_vis import prettyPicture
-### from prep_terrain_data import makeTerrainData
+import sys
+from class_vis import prettyPicture
+from prep_terrain_data import makeTerrainData
 
 import matplotlib.pyplot as plt
 import copy
